1c1027a48272e79fde3c8200a3993366380bc501
Abdi <abdi.elmi@rocketmail.com>
2022-09-25 14:02:09 -0400

    cleaned js, css and added copy funtion

--- script.js
+++ script.js
@@ -1,29 +1,56 @@
-// Create an EyeDropper object
 let eyeDropper = new EyeDropper();
-let colourValueText = document.getElementsByClassName("colour-value");
-let selectedColour ="";
-// todo: save to cookies for next time user returns
-let colourHistory = [];
 
-document.getElementById("eyedropper").addEventListener('click', e => {
-    eyeDropper.open()
-    .then(colorSelectionResult => {
-        // returns hex color value (#RRGGBB) of the selected pixel
-        console.log(colorSelectionResult.sRGBHex);
-        selectedColour = colorSelectionResult.sRGBHex;
-        $(".colour-preview").css("background", selectedColour)
-        $(".colour-value").text(selectedColour)
-        if(colourHistory.length < 5) {
-          colourHistory.push(selectedColour);
-          console.log(colourHistory)
-        } else {
-          colourHistory.shift()
-          colourHistory.push(selectedColour);
-          console.log(colourHistory);
-        }
-      })
-      .catch(error => {
-        // handle the user choosing to exit eyedropper mode without a selection
-        console.log(error)
-      });
+let selectedColour ="";
+let colourValueText =  $(".colour-value");
+let colourValuePreview = $(".colour-preview");
+let eyeDropperButton = $("#eyeDropper");
+let copiedAlertDiv = $(".copiedAlert");
+
+let colourHistoryArray = [];
+let addingHTMLArray = [];
+
+$(document).ready(function() {
+  // copiedAlertDiv.css("height", 0);
+  eyeDropperButton.on("click", function(e) {
+    eyeDpper();
+  });
+
+  colourValuePreview.on("click", function(e) {
+    copyValue();
+  })
+});
+
+const eyeDpper = () => {
+  eyeDropper.open()
+  .then(colorSelectionResult => {
+      // returns hex color value (#RRGGBB) of the selected pixel
+      // console.log(colorSelectionResult.sRGBHex);
+      selectedColour = colorSelectionResult.sRGBHex;
+      colourValuePreview.css("background", selectedColour)
+
+      colourValueText.text(selectedColour)
+
+      if(colourHistoryArray.length < 5) {
+        colourHistoryArray.push(selectedColour);
+        console.log(colourHistoryArray)
+      } else {
+        colourHistoryArray.shift()
+        colourHistoryArray.push(selectedColour);
+        console.log(colourHistoryArray);
+      }
+    })
+    .catch(error => {
+      // handle the user choosing to exit eyedropper mode without a selection
+      console.log(error)
     });
+}
+
+const copyValue = () => {
+  navigator.clipboard.writeText(colourValueText.text()).then(function() {
+    console.log('Async: Copying to clipboard was successful!');
+    copiedAlertDiv.css("background", colourValueText.text())
+    copiedAlertDiv.show("slow").delay(500).hide("slow");
+  }, function(err) {
+    console.error('Async: Could not copy text: ', err);
+  });
+}
